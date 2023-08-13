ff0402f2e8c336e7fd5899bea9c4ce7dbac1a52c
Amit Singh Rana <ranaa73000@gmail.com>
2023-08-13 13:46:11 +0530

    Connected to MongoDB database && Setup .env

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 // Import Packages
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 // Import files-functions
 import apiRoutes from "./routes/index.js";
 
 const app = express();
 
-// Middlewares
+/*
+    Middlewares
+*/
+dotenv.config(); // To process some sensitive info. from env variables
 app.use(express.json()); // To parse the form data as JSON
 
 // Attach API Routes
 app.use("/api", apiRoutes);
 
-// Server Setup
+/*
+    Mongoose DB connection
+*/
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Connection Error ", err.message));
+
+/*
+    Server Setup
+*/
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
